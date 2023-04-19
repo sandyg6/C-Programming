d226eee53281dcfe7af6bf49e1b5fd1dbf045e0f
SANDHIYA G <118624179+sandyg6@users.noreply.github.com>
2023-04-19 18:46:31 +0530

    Create navg.c

---
+++ navg.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int num, count, navg=0;
+    float x, average, sum=0;
+    printf("How many numbers?");
+    scanf("%d",&num);
+    count=1;
+    do
+    {
+        printf("x=");
+        scanf("%f",&x);
+        if(x<0)
+        {
+            printf("ERROR-NEGATIVE VALUE FOR x \n");
+            continue;
+        }
+        sum+=x;
+        ++navg;
+        count++;
+    }
+    while(count<=num);
+    average=sum/navg;
+    printf("\n The average is %f \n",average);
+}
