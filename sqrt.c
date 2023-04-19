9df1182f30fcc9cddaf853276fd9dea4ddbafb05
SANDHIYA G <118624179+sandyg6@users.noreply.github.com>
2023-04-19 18:57:10 +0530

    Create sqrt.c

---
+++ sqrt.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    float num,res;
+    input:
+    printf("Enter a number(to end press -ve value):");
+    scanf("%f",&num);
+    if(num<=0)
+    goto end;
+    res=sqrt(num);
+    printf("The  square root of %f is %f\n",num,res);
+    goto input;
+    end:
+    printf("Press any key");
+}
