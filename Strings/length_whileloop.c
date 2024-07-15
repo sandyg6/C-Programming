d8d99620bcb93f7160db1ca67e2d28b48d2bff5e
Sandhiya <118624179+sandyg6@users.noreply.github.com>
2024-07-15 04:48:03 +0000

    Add files

--- Strings/length_whileloop.c
+++ Strings/length_whileloop.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#define MAX_SIZE 100
 
+int main()
+{
+    char text[MAX_SIZE];
+    int index= 0;
+    printf("Enter any string: ");
+    gets(text);
+    while(text[index] != '\0')
+    {
+        index++;
+    }
+    printf("Length of '%s' = %d", text, index);
+    return 0;
+}
