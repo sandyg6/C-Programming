6512a5176b9665aef5e25707469ccb676430bbed
Sandhiya <118624179+sandyg6@users.noreply.github.com>
2024-07-22 23:44:34 +0530

    Update Pr0blem2.c

--- Arrays/Pr0blem2.c
+++ Arrays/Pr0blem2.c
@@ -1,20 +1,27 @@
 #include<stdio.h>
 
 int main(){
-    int money;
-    scanf("%d",&money);
+    int n;
+    scanf("%d",&n);
+    int money[n];
+    for(int m=0;m<n;m++){
+        scanf("%d",&money[m]);
+    }
     int values[] = {500,100,50,20,10,5,2,1};
     int count[8] = {0};
-    int i=0;
-    while(money>0){
-        while(money >= values[i]){
-            count[i]++;
-            money = money - values[i];
+    for(int l=0;l<n;l++){
+        int i=0;
+        while(money[l]>0){
+            while(money[l] >= values[i]){
+                count[i]++;
+                money[l] = money[l] - values[i];
+            }
+            i++;
         }
-        i++;
-    }
-    for(int j=0;j<8;j++){
-        printf("%d:%d\n",values[j],count[j]);
+        for(int j=0;j<8;j++){
+            printf("%d:%d\n",values[j],count[j]);
+        }
+        printf("\n");
     }
     return 0;
 }
