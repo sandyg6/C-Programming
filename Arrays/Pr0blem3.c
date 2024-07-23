350d28e4e58d2ae213186b85eb4541d38d6f6922
Sandhiya <118624179+sandyg6@users.noreply.github.com>
2024-07-23 20:11:45 +0530

    Create Pr0blem3.c

---
+++ Arrays/Pr0blem3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <stdlib.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    int sub[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        sub[i]=0;
+    }
+    for(int j=0;j<n;j++){
+        int k = j+1;
+        sub[j]= abs(arr[k] - arr[j]);
+    }
+    int min = 0;
+    for(int l=1;l<n;l++){
+        if(sub[min]>sub[l]){
+            min = l;
+        }
+    }
+    printf("%d",sub[min]);
+}
